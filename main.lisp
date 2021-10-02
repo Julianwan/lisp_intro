2744197b782852e967cf8099d9529d0062bcac8d
Julian Wan <juliangwan@gmail.com>
2021-10-01 22:46:24 -0500

    Cute

---
+++ main.lisp
@@ -0,0 +1,14 @@
+(defun triple 
+  (X)
+"Compute three times X."
+  (* 3 X))                  
+
+(defun negate 
+  (X)
+"Negate the value of X."
+  (- X))
+
+(defun quintuple   
+  (X)
+"Quintuple the value of X"
+  (* 5 X)) 
---
+++ readme.md
@@ -0,0 +1,9 @@
+# I'm trying to start with lisp
+
+rlwrap ros run?
+
+Not sure what I'm doing.
+
+- ros init hello-world
+- ros hello-world.ros
+- ros build hello-world.ros && ./hello-world
